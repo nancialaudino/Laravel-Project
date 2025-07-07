35723da6f8556ceb86f61c1cfb23e18a99c4824b
Nancia Laudino <nancialaudino@gmail.com>
2025-07-07 15:02:04 +0200

    Added URL setup in the route file

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -7,8 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-*/
-
 Route::get('/', function () {
     return view('hello', [
         'laravelVersion' => app()->version(),
@@ -16,3 +14,24 @@ Route::get('/', function () {
     ]);
 });
 
+*/
+
+// Page d'accueil
+Route::get('/', function () {
+    return 'Hello World From Laravel';
+});
+
+// Liste des produits
+Route::get('/produits', function () {
+    return 'Voici la liste des produits';
+});
+
+// Détails d’un produit
+Route::get('/produits/{id}', function ($id) {
+    return "Détails du produit avec l'ID : $id";
+});
+
+// Panier
+Route::get('/panier', function () {
+    return 'Voici votre panier';
+});
